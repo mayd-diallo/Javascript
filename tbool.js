63d60e44a56a82c5e77b301b785cb54f0e4909cf
mayd-diallo <maydiallo78@gmail.com>
2025-07-31 12:14:04 +0200

    boucle js

---
+++ tbool.js
@@ -0,0 +1,4 @@
+ villes = [ "paris" ,"lyon" , "marseille" , "toulouse" , "nice" , "nantes" , "strasbourg" , "montpellier" , "bordeaux" , "lille" ];
+ villes.forEach(function(ville) {
+    console.log(ville);
+})
